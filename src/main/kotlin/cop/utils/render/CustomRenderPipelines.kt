18845e80ce7d844b344d4df6243844d93820edb3
elv1n200 <elvinsubasi01@gmail.com>
2026-04-29 17:04:21 +0200

    Fix world-render pipelines on 1.21.11 (line vertex format)

--- src/main/kotlin/cop/utils/render/CustomRenderPipelines.kt
+++ src/main/kotlin/cop/utils/render/CustomRenderPipelines.kt
@@ -11,13 +11,19 @@ import net.minecraft.client.renderer.RenderPipelines
  * from OdinFabric (BSD 3-Clause)
  * copyright (c) 2025-2026 odtheking
  * original: https://github.com/odtheking/OdinFabric/blob/main/src/main/kotlin/com/odtheking/odin/utils/render/CustomRenderPipelines.kt
+ *
+ * The vertex format on the LINE_* pipelines is intentionally **not** overridden.
+ * 1.21.11 introduced `POSITION_COLOR_NORMAL_LINE_WIDTH` and the per-vertex
+ * `setLineWidth()` shader path, while 1.21.10 still uses `POSITION_COLOR_NORMAL`.
+ * `LINES_SNIPPET` already wires the right format for whichever version we're
+ * compiled against, so leaving the format unset prevents the snippet's choice
+ * from being clobbered.
  */
 object CustomRenderPipelines {
 
     val LINE_LIST: RenderPipeline = RenderPipelines.register(
         RenderPipeline.builder(*arrayOf<RenderPipeline.Snippet>(RenderPipelines.LINES_SNIPPET))
-            .withLocation("pipeline/lines")
-            .withVertexFormat(DefaultVertexFormat.POSITION_COLOR_NORMAL, VertexFormat.Mode.LINES)
+            .withLocation("cop/pipeline/lines")
             .withCull(false)
             .withBlend(BlendFunction.TRANSLUCENT)
             .withDepthWrite(true)
@@ -27,9 +33,8 @@ object CustomRenderPipelines {
 
     val LINE_LIST_ESP: RenderPipeline = RenderPipelines.register(
         RenderPipeline.builder(*arrayOf<RenderPipeline.Snippet>(RenderPipelines.LINES_SNIPPET))
-            .withLocation("pipeline/lines")
+            .withLocation("cop/pipeline/lines_esp")
             .withShaderDefine("shad")
-            .withVertexFormat(DefaultVertexFormat.POSITION_COLOR_NORMAL, VertexFormat.Mode.LINES)
             .withCull(false)
             .withBlend(BlendFunction.TRANSLUCENT)
             .withDepthWrite(false)
@@ -39,7 +44,7 @@ object CustomRenderPipelines {
 
     val TRIANGLE_STRIP: RenderPipeline = RenderPipelines.register(
         RenderPipeline.builder(*arrayOf<RenderPipeline.Snippet>(RenderPipelines.DEBUG_FILLED_SNIPPET))
-            .withLocation("pipeline/debug_filled_box")
+            .withLocation("cop/pipeline/debug_filled_box")
             .withCull(false)
             .withVertexFormat(DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.TRIANGLE_STRIP)
             .withDepthWrite(true)
@@ -50,7 +55,7 @@ object CustomRenderPipelines {
 
     val TRIANGLE_STRIP_ESP: RenderPipeline = RenderPipelines.register(
         RenderPipeline.builder(*arrayOf<RenderPipeline.Snippet>(RenderPipelines.DEBUG_FILLED_SNIPPET))
-            .withLocation("pipeline/debug_filled_box")
+            .withLocation("cop/pipeline/debug_filled_box_esp")
             .withCull(false)
             .withVertexFormat(DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.TRIANGLE_STRIP)
             .withDepthWrite(false)
